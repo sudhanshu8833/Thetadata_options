568feaecec2f0dbba38c1c4b3a98fe886a256ba8
sudhanshu <sudhanshus883@gmail.com>
2024-03-14 10:25:57 +0530

    hell

--- core/strategy.py
+++ core/strategy.py
@@ -1,4 +1,6 @@
 import requests
+import aiohttp
+import asyncio
 from datetime import datetime,timedelta
 import pandas as pd
 from pprint import pprint
@@ -30,36 +32,65 @@ class thetaData:
         self.day=False
 
 
-    def get_expirations(self, instrument):
-        response=requests.get(f"http://127.0.0.1:25510/v2/list/expirations?root={instrument}").json()['response']
-        return response
+    async def get_expirations(self, session, instrument):
+        url = f"http://127.0.0.1:25510/v2/list/expirations?root={instrument}"
+        async with session.get(url) as response:
+            response=await response.json()
+            return response['response']
 
-    def get_bulk_quotes(self, instrument, expiry):
-        return requests.get(f"http://127.0.0.1:25510/bulk_snapshot/option/quote?root={instrument}&exp={expiry}").json()['response']
+    async def get_bulk_quotes(self, session, instrument, expiry):
+        url = f"http://127.0.0.1:25510/bulk_snapshot/option/quote?root={instrument}&exp={expiry}"
+        async with session.get(url) as response:
+            response=await response.json()
+            return response['response']
 
-    def get_ticker_price(self,instrument):
+    async def get_ticker_price(self, session, instrument):
         if self.day:
             return data['json_data']['stock_price'][instrument]
-        return requests.get(f"http://127.0.0.1:25510/v2/snapshot/stock/trade?root={instrument}").json()['response']
+        url = f"http://127.0.0.1:25510/v2/snapshot/stock/trade?root={instrument}"
+        async with session.get(url) as response:
+            response=await response.json()
+            return response['response']
+
 
     def convert_to_YYYMMDD(self, date):
         return date.strftime("%Y%m%d")
 
-    def get_EOD(self,instrument,expiry,strike,right):
+    def get_url(self,instrument,expiry,strike,right):
         today_date=self.convert_to_YYYMMDD(datetime.now())
         four_days_back=self.convert_to_YYYMMDD(datetime.now()-timedelta(days=4))
-        response = requests.get(f"http://127.0.0.1:25510/v2/hist/option/eod?root={instrument}&exp={expiry}&strike={strike}&right={right}&start_date={four_days_back}&end_date={today_date}").json()
-        response=response['response'][-1][5]
-        return response
+        return f"http://127.0.0.1:25510/v2/hist/option/eod?root={instrument}&exp={expiry}&strike={strike}&right={right}&start_date={four_days_back}&end_date={today_date}"
 
-    def option_trade_price(self, instrument, expiry, contract, strike):
+
+    async def get_EOD(self,session,instrument,expiry,strike,right,type):
+        today_date=self.convert_to_YYYMMDD(datetime.now())
+        four_days_back=self.convert_to_YYYMMDD(datetime.now()-timedelta(days=4))
+        url=""
+        if type=="option":
+            url=f"http://127.0.0.1:25510/v2/hist/{type}/eod?root={instrument}&exp={expiry}&strike={strike}&right={right}&start_date={four_days_back}&end_date={today_date}"
+            async with session.get(url) as response:
+                response= await response.json()
+                response=response['response'][-1][5]    
+                return response
+        else:
+            url=f"http://127.0.0.1:25510/hist/stock/eod?root={instrument}&start_date={four_days_back}&end_date={today_date}"
+            async with session.get(url) as response:
+                response= await response.json()
+                response=response['response'][-1][3]    
+                return response
+
+    async def option_trade_price(self,session, instrument, expiry, contract, strike):
         if str(expiry) == "nan":
             return 0
-        response=requests.get(f"http://127.0.0.1:25510/snapshot/option/trade?root={instrument}&exp={expiry}&right={contract}&strike={strike}").json()['response']
-        if(response[0]==0):
-            return 0
-        else:
-            return response[0][-2]
+        url = f"http://127.0.0.1:25510/snapshot/option/trade?root={instrument}&exp={expiry}&right={contract}&strike={strike}"
+
+        async with session.get(url) as response:
+            response=await response.json()
+            response=response['response']
+            if(response[0]==0):
+                return 0
+            else:
+                return response[0][-2]
 
 
     def convert_to_datetime(self, date_value):
@@ -68,7 +99,7 @@ class thetaData:
         return date_obj
 
 
-    def manage_quotes(self,instrument,expiry):
+    async def manage_quotes(self,session,instrument,expiry):
         if(self.convert_to_datetime(expiry)<datetime.now()):
             return
 
@@ -83,7 +114,7 @@ class thetaData:
                 return
 
         else:
-            quotes=self.get_bulk_quotes(instrument,expiry)
+            quotes=await self.get_bulk_quotes(session,instrument,expiry)
             if(quotes[0]==0):
                 return
             json_data={}
@@ -95,26 +126,27 @@ class thetaData:
             self.json_data[instrument][expiry]=json_data
 
 
-        call=self.option_trade_price(instrument,expiry,'C',closest_key)
+        call=await self.option_trade_price(session,instrument,expiry,'C',closest_key)
         if(call!=0):
             self.json_data[instrument][expiry][closest_key]['C']=call
         else:
-            self.json_data[instrument][expiry][closest_key]['C']=self.get_EOD(instrument,expiry,closest_key,'C')
+            self.json_data[instrument][expiry][closest_key]['C']=await self.get_EOD(session,instrument,expiry,closest_key,'C',"option")
 
-        put=self.option_trade_price(instrument,expiry,'P',closest_key)
+        put=await self.option_trade_price(session,instrument,expiry,'P',closest_key)
         if(put!=0):
             self.json_data[instrument][expiry][closest_key]['P']=put
         else:
-            self.json_data[instrument][expiry][closest_key]['P']=self.get_EOD(instrument,expiry,closest_key,'P')
+            self.json_data[instrument][expiry][closest_key]['P']=await self.get_EOD(session,instrument,expiry,closest_key,'P',"option")
         
 
-    def base_called(self,instrument):
-        expirations=self.get_expirations(instrument)
+    async def base_called(self,session,instrument):
+        expirations=await self.get_expirations(session,instrument)
         self.json_data[instrument]={}
         for expiry in expirations:
-            self.manage_quotes(instrument,str(expiry))
+            await self.manage_quotes(session,instrument,str(expiry))
+        # await asyncio.gather(*[self.manage_quotes(session,instrument, str(expiry)) for expiry in expirations])
 
-    def update_stock_price(self,instrument):
+    async def update_stock_price(self,session,instrument):
         if (self.day):
             try:
                 self.stock_price[instrument]=data['stock_price'][instrument]
@@ -122,12 +154,11 @@ class thetaData:
             except:
                 self.stock_price[instrument]="NA"
                 return
-
         try:
-            response=self.get_ticker_price(instrument)
+            response=await self.get_ticker_price(session,instrument)
             self.stock_price[instrument]=response[0][-2]
         except:
-            self.stock_price[instrument]="NA"
+            self.stock_price[instrument]=await self.get_EOD(session,instrument,"NA","NA",'C',"stock")
 
 
     def closest_strike(self,dictionary, value):
@@ -139,13 +170,18 @@ class thetaData:
         temp_list.append(self.stock_price[instrument])
 
         for expiry,strikes in value.items():
-            closest_key=self.closest_strike(strikes,self.stock_price[instrument])
+            closest_key=self.closest_strike(strikes,self.stock_price[instrument]*1000)
             call=strikes[closest_key]['C']
             put=strikes[closest_key]['P']
             temp_list.append(call)
             temp_list.append(put)
             percentage_difference=((call-put)/self.stock_price[instrument])*100
             temp_list.append(percentage_difference)
+            temp_list.append(str(closest_key))
+            temp_list.append(str(expiry))
+
+            temp_list.append(self.get_url(instrument,expiry,closest_key,'C'))
+
 
 
         return temp_list
@@ -160,12 +196,18 @@ class thetaData:
         for i in range(len(df.columns)):
             if i == 0:
                 new_columns[i] = 'ticker_price'
-            elif (i - 1) % 3 == 0:
-                new_columns[i] = f'call_{(i - 1) // 3 + 1}'
-            elif (i - 1) % 3 == 1:
-                new_columns[i] = f'put_{(i - 1) // 3 + 1}'
+            elif (i - 1) % 6 == 0:
+                new_columns[i] = f'call_{(i - 1) // 6 + 1}'
+            elif (i - 1) % 6 == 1:
+                new_columns[i] = f'put_{(i - 1) // 6 + 1}'
+            elif (i - 1) % 6 == 2:
+                new_columns[i] = f'difference_{(i - 1) // 6 + 1}'
+            elif (i - 1) % 6 == 3:
+                new_columns[i] = f'strike_{(i - 1) // 6 + 1}'
+            elif (i - 1) % 6 == 4:
+                new_columns[i] = f'expiry_{(i - 1) // 6 + 1}'
             else:
-                new_columns[i] = f'difference_{(i - 1) // 3 + 1}'
+                new_columns[i] = f'url_{(i - 1) // 6 + 1}'
 
         print(df)
         df.rename(columns=new_columns, inplace=True)
@@ -178,22 +220,26 @@ class thetaData:
         sorted_df.to_csv(file_path)
         self.df=sorted_df
 
-    def main(self):
+    async def gather_calls(self, session, ticker):
+        try:
+            print(ticker)
+            if ticker in blocked_ticker:
+                return
+
+            await self.update_stock_price(session, ticker)
+            await self.base_called(session, ticker)
+        except Exception as e:
+            print(traceback.format_exc())
+
+    async def main(self):
         tickers=requests.get(self.get_roots_url).json()['response']
 
-        for ticker in tickers[:90]:
-            # try:
-                print(ticker)
-                if ticker in blocked_ticker:
-                    continue
+        async with aiohttp.ClientSession() as session:
+            tickers = requests.get(self.get_roots_url).json()['response']
+            # tickers=['AFTY']
+            await asyncio.gather(*[self.gather_calls(session, arg) for arg in tickers])
 
-                self.update_stock_price(ticker)
-                self.base_called(ticker)
-                if (ticker=="AA"):
-                    break
 
-            # except:
-            #     print(f"Exception came for {ticker}")
 
         self.final_list={}
         for key,value in self.json_data.items():
@@ -216,5 +262,5 @@ class thetaData:
 if __name__=="__main__":
     the=thetaData()
     time_now=time.time()
-    the.main()
+    asyncio.run(the.main())
     print(time.time()-time_now)
